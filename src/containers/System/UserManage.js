2d73b43150f0d1ace0bbbeb2879b69d485bf2092
nini <bploveme048@gmail.com>
2025-08-16 22:33:37 +0700

    all users

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,79 @@
-import React, { Component } from 'react';
+import { async } from 'q';
+import React, { Component, useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
-class UserManage extends Component {
+import { getAllUsers } from '../../services/userService';
+const UserManage = (props) => {
 
-    state = {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
 
+    const fetchUsers = async () => {
+      try {
+
+        let res = await getAllUsers('ALL')
+        if (res && res.errorCode === 0) {
+          setUsers(res.users)
+        }
+        else if (res) {
+          console.log('>>>res error: ', res)
+        }
+      }
+      catch (e) {
+        console.log('>>>error:', e)
+
+      }
     }
+    fetchUsers()
+  }, [])
+  return (
+    <div className="manage-users">
+      <div className="title text-center"> Manage Users
+      </div>
+      <div className="users-table">
+        <table className="table table-hover table-striped w-75 mx-auto mt-5">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Email</th>
+              <th scope="col">Firstname</th>
+              <th scope="col">Lastname</th>
+              <th scope="col">Address</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody >
+            {users && users.map(user => (
+              <tr key={user.id}>
+                <th scope="row">
+                  {user.id}
+                </th>
+                <td>
+                  {user.email}
+                </td>
+                <td>
+                  {user.firstName}
+                </td>
+                <td>
+                  {user.lastName}
+                </td>
+                <td>
+                  {user.address}
+                </td>
+                <td>
+                  <a href="" role='button' className="btn btn-outline-secondary">Edit</a>
+                  <a href="" className="btn btn-outline-danger" role='button'>Delete</a>
+                </td>
+              </tr>
+            ))}
 
-    componentDidMount() {
 
-    }
+          </tbody>
+        </table>
+      </div>
 
 
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+    </div>
+  );
 }
-
-const mapStateToProps = state => {
-    return {
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
+export default UserManage;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,8 @@
 import axios from "../axios";
-
 export const postLogin = (email, password) => {
   return axios.post('/api/login', { email, password })
 
+}
+export const getAllUsers = (id) => {
+  return axios.get(`api/get-all-users?id=${id}`)
 }
