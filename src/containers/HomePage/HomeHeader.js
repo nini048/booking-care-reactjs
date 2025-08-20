3b7db85b272597ec283057bf20cd4387a2830d5c
nini <bploveme048@gmail.com>
2025-08-20 20:40:55 +0700

    envi

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -8,10 +8,16 @@ import { IoMdMenu } from "react-icons/io";
 import { FaRegQuestionCircle } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
 import { FaRegHospital, FaMicroscope, FaBrain, FaTooth, FaHospital, FaMobile } from "react-icons/fa";
+import logo from '../../assets/images/logo.png'
+import { FormattedMessage, useIntl } from 'react-intl';
 
-const HomeHeader = () => {
+
+const HomeHeader = (props) => {
+  const intl = useIntl();
+
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-
+  console.log(useSelector((state) => state.user))
+  const language = useSelector((state) => state.user.language)
   const linkToRedirect = isLoggedIn ? '/system/user-manage' : '/home';
 
   return (
@@ -19,86 +25,123 @@ const HomeHeader = () => {
       <div className='home-header-container'>
         <div className='home-header-content'>
           <div className='left-content'>
-            <IoMdMenu size={32} color='grey' cursor='pointer' />
+            <IoMdMenu size={24} cursor='pointer' />
+            <img src={logo} />
             <div className='header-logo'>
 
             </div>
           </div>
           <div className='center-content'>
             <div className="child-content">
-              <div className='subs-title'><b>Chuyên khoa</b></div>
-              <div>Tìm bác sĩ theo chuyên khoa</div>
+              <div className='subs-title'>
+                <b><FormattedMessage id='home-header.specialty' /></b>
+              </div>
+              <div><FormattedMessage id="home-header.search-doctor" /></div>
+
             </div>
 
             <div className="child-content">
-              <div className='subs-title'><b>Cơ sở y tế</b></div>
-              <div>Chọn bệnh viện, phòng khám</div>
+              <div className='subs-title'><b>
+                <FormattedMessage id='home-header.health-facility' />
+              </b></div>
+              <div>
+                <FormattedMessage id='home-header.select-hospital' />
+              </div>
             </div>
 
             <div className="child-content">
-              <div className='subs-title'><b>Bác sĩ</b></div>
-              <div>Chọn bác sĩ giỏi</div>
+              <div className='subs-title'><b>
+                <FormattedMessage id='home-header.doctor' />
+
+              </b></div>
+              <div>
+                <FormattedMessage id='home-header.select-doctor' />
+
+              </div>
             </div>
 
             <div className="child-content">
-              <div className='subs-title'><b>Gói khám</b></div>
-              <div>Khám sức khỏe tổng quát</div>
+              <div className='subs-title'><b>
+                <FormattedMessage id='home-header.fee' />
+              </b></div>
+              <div>
+                <FormattedMessage id='home-header.general-health-check' />
+              </div>
             </div>
 
           </div>
 
           <div className='right-content'>
             <div className='support'>
-              <FaRegQuestionCircle size={20} color='grey' cursor='pointer' />
-              Hỗ trợ
+              <FaRegQuestionCircle size={18} cursor='pointer' />
+              <FormattedMessage id='home-header.support' />
             </div>
-            <div className='flag'>VI</div>
+            <div className='language-vi active'>VI</div>
+            <div className='language-en'>EN</div>
           </div>
         </div>
       </div>
       <div className='home-header-banner'>
-        <div className='title1'>NỀN TẢNG Y TẾ</div>
-        <div className='title2'>CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+        <div className='title1'>
+          <FormattedMessage id='banner.title1' />
+        </div>
+        <div className='title2'>
+          <FormattedMessage id='banner.title2' />
+        </div>
         <div className='search'>
           <FaSearch />
-          <input type='text' placeholder='Tìm chuyên khoa khám bệnh' />
+          <input type='text'
+            placeholder={intl.formatMessage({ id: 'search.placeholder' })} />
         </div>
         <div className='options'>
           <div className='option-child'>
             <div className='icon-child'>
               <FaRegHospital size={30} />
             </div>
-            <div className='text-child'>Khám chuyên khoa</div>
+            <div className='text-child'>
+              <FormattedMessage id='option.specialist' />
+            </div>
           </div>
           <div className='option-child'>
             <div className='icon-child'>
               <FaMobile size={30} />
             </div>
-            <div className='text-child'>Khám từ xa</div>
+            <div className='text-child'>
+              <FormattedMessage id='option.remote' />
+            </div>
           </div>
           <div className='option-child'>
             <div className='icon-child'>
               <FaHospital size={30} />
             </div>
-            <div className='text-child'>Khám tổng quát</div>
+            <div className='text-child'>
+              <FormattedMessage id='option.general' />
+
+            </div>
           </div>
           <div className='option-child'>
             <div className='icon-child'>
               <FaMicroscope size={30} />
             </div>
-            <div className='text-child'>Xét nghiệm y học</div>
+            <div className='text-child'>
+              <FormattedMessage id='option.lab' />
+            </div>
           </div>
           <div className='option-child'>
             <div className='icon-child'>
               <FaBrain size={30} />
             </div>
-            <div className='text-child'>Sức khoẻ tinh thần</div>
+            <div className='text-child'>
+              <FormattedMessage id='option.mental' />
+            </div>
           </div>
           <div className='option-child'>
             <div className='icon-child'>
               <FaTooth size={30} />
             </div>
-            <div className='text-child'>Khám nha khoa</div>
+            <div className='text-child'>
+              <FormattedMessage id='option.dental' />
+            </div>
           </div>
         </div>
       </div>
